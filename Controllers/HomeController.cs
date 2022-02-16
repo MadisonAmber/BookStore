51fbcfbfacb8a9847cf4c0d992efce438078f6e4
Madison Clark <madamber4@gmail.com>
2022-02-15 19:07:40 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using BookStore.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookProjectRepository repo;
+
+        public HomeController(IBookProjectRepository repository)
+        {
+            repo = repository;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize);
+
+            BooksViewModel vm = new BooksViewModel
+            {
+                Books = books,
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Models/EFBookProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class EFBookProjectRepository : IBookProjectRepository
+    {
+        private BookstoreContext context { get; set; }
+        public EFBookProjectRepository(BookstoreContext ctx) => context = ctx;
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
