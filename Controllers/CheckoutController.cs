77de280c1770d0432b51845cb9cb4a779e7af66c
Madison Clark <madamber4@gmail.com>
2022-03-01 20:03:07 -0700

    Mission 9 Submission

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,49 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private IPurchaseRepository repo { get; set; }
+
+        private Basket basket { get; set; }
+
+        public CheckoutController(IPurchaseRepository x, Basket b)
+        {
+            repo = x;
+            basket = b;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View(new Purchase());
+        }
+        [HttpPost]
+        public IActionResult Checkout(Purchase p)
+        {
+            if(basket.Items.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                p.Lines = basket.Items.ToArray();
+                repo.SavePurchase(p);
+                basket.ClearCart();
+                return RedirectToPage("/PurchaseConfirmation");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
